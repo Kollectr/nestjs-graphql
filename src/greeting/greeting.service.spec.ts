9bc329ea53e9e5e6fa5210279d0fb861b802790b
jukafah <jukafah@proton.me>
2024-04-15 17:22:56 -0500

    remove test

--- src/greeting/greeting.service.spec.ts
+++ src/greeting/greeting.service.spec.ts
@@ -10,8 +10,4 @@ describe('GreetingService', () => {
   it('should return a correct greeting message', () => {
     expect(service.getGreeting('World')).toBe('Hello, World!');
   });
-
-  it('should return a correct goodbye message', () => {
-    expect(service.getGoodbye('World')).toBe('Goodbye, World!');
-  });
 });
